996fd4f3f6b21af71d134f41a6d93760ecdeaa40
PRATIK12NIPANE <136259872+PRATIK12NIPANE@users.noreply.github.com>
2025-03-19 19:03:45 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://placeinformer.onrender.com/api";
 
 export const registerUser = async (userData) => {
   return axios.post(`${API_URL}/auth/register`, userData);
