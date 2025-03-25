3e7053f9361b2f3b9d083f71afd954c2c57b6d72
PRATIK12NIPANE <136259872+PRATIK12NIPANE@users.noreply.github.com>
2025-03-25 15:27:41 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,30 +1,43 @@
 import axios from "axios";
 
-const API_URL = "https://placeinformer.onrender.com/api";
+const API_URL = "http://localhost:5000/api";
 
+// Register User
 export const registerUser = async (userData) => {
   return axios.post(`${API_URL}/auth/register`, userData);
 };
 
+// Login User
 export const loginUser = async (userData) => {
   return axios.post(`${API_URL}/auth/login`, userData);
 };
 
-export const getUserProfile = async (token) => {
+// Get User Profile
+export const getUserProfile = async () => {
+  const token = localStorage.getItem("token");
   return axios.get(`${API_URL}/user/profile`, {
     headers: { Authorization: `Bearer ${token}` },
   });
 };
 
-
+// Update User Profile
 export const updateUserProfile = async (userData) => {
   const token = localStorage.getItem("token");
-  return fetch("/api/update-profile", {
-    method: "POST",
-    headers: {
+  return axios.put(`${API_URL}/user/profile`, userData, {
+    headers: { 
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     },
-    body: JSON.stringify(userData),
-  }).then((res) => res.json());
+  });
+};
+
+// Create New User (Admin Functionality)
+export const createUser = async (userData) => {
+  const token = localStorage.getItem("token");
+  return axios.post(`${API_URL}/user/create`, userData, {
+    headers: { 
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
 };
